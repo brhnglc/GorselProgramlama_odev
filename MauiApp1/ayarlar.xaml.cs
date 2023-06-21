3cb8d436ecc32ef54325fa02df60b06b4e7a21d7
brhnglc <brhnglcc@hotmail.com>
2023-06-21 16:38:46 +0300

    Add project files.

---
+++ MauiApp1/ayarlar.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1;
+
+public partial class ayarlar : ContentPage
+{
+	public ayarlar()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiApp1/yapılıcakalar.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class yapılıcakalar : ContentPage
+{
+	public yapılıcakalar()
+	{
+		InitializeComponent();
+	}
+}
